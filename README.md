82390db6fd26ef5103b77dda24c4a6cf52014ffe
Yaroslav Savchuk <ysavchuk@heliostech.fr>
2018-02-20 22:18:59 +0200

    Zsh & oh-my-zsh installation

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 ⚙️ My configuration files for unix based systems
 
 [Thanks to this repo](https://github.com/webpro/dotfiles)
+
+## install.sh expects zsh to be installed in you system
--- install.sh
+++ install.sh
@@ -1,21 +1,29 @@
 #!/usr/bin/env bash
 
-# Get current dir (so run this script from anywhere)
+# Check for zsh installation
+if ! is-executable zsh; then
+  echo 'Install zsh first'; exit 1;
+fi
 
+# Update or install oh-my-zsh
+if is-executable upgrade_oh_my_zsh; then
+  upgrade_oh_my_zsh
+else
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+fi
+
+# Get current dir (so run this script from anywhere)
 export DOTFILES_DIR DOTFILES_CACHE DOTFILES_EXTRA_DIR
 DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 DOTFILES_CACHE="$DOTFILES_DIR/.cache.sh"
 
 # Make utilities available
-
 PATH="$DOTFILES_DIR/bin:$PATH"
 
 # Update dotfiles itself first
-
 #if is-executable git -a -d "$DOTFILES_DIR/.git"; then git --work-tree="$DOTFILES_DIR" --git-dir="$DOTFILES_DIR/.git" pull origin master; fi
 
 # Bunch of symlinks
-
 ln -sfv "$DOTFILES_DIR/.zshrc" ~
 ln -sfv "$DOTFILES_DIR/.gitconfig" ~
 ln -sfv "$DOTFILES_DIR/.gitignore_global" ~
